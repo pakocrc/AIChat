008b0ddc97533587120643db1d619e0eb38af067
Francisco Cordoba <pako.cordoba10@gmail.com>
2025-01-09 20:16:54 -0600

    First commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Francisco Cordoba on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
